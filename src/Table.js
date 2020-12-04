e7ccd108f73f8306f244b16bc5abe70c0f188f20
Oluwaseun <ekunkoya.seun@gmail.com>
2020-12-04 16:37:48 +0100

    commit1

---
+++ src/Table.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    )
+  }
+
+  const TableBody = () => {
+    return (
+      <tbody />
+    )
+  } 
+
+class Table extends Component {
+  render() {
+      const {characterData} = this.props
+    return (
+      <table>
+          <TableHeader />
+          <TableBody characterData = {characterData}/>
+      </table>
+    )
+  }
+}
+
+export default Table
